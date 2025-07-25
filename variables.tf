b908d1cbf7c251d6c421e611bb1754e82fb420b4
NipurJain4 <nipurjain.tmu.cs@gmail.com>
2025-07-25 18:37:57 +0530

    variables file added

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ec2_instance-type" {
+    default = "t2.micro"
+    type = string
+}
+
+variable "ec2_root_storage_size" {
+    default = 10
+    type = number
+}
+
+variable "ec2_ami" {
+    default = "ami-0d0ad8bb301edb745"
+    type = string
+}
+
+variable "ec2_instance_name" {
+    default = "instance_for_DAS"
+    type = string
+}
